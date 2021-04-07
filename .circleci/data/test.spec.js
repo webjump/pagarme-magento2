51f60cd346f771488c15c452828bb0b0979f9d90
netorodrigues <jose.oneto@mundipagg.com>
2021-04-07 15:02:15 -0300

    :alembic: changes test to register an user

--- .circleci/data/test.spec.js
+++ .circleci/data/test.spec.js
@@ -2,7 +2,7 @@
 
 context('Actions', () => {
 
-  Cypress._.times(100,() => {
+  /*Cypress._.times(1,() => {
   it('should add something to cart', () => {
     cy.visit('https://stg-magento2.mundipagg.com/push-it-messenger-bag.html');
 
@@ -15,7 +15,35 @@ context('Actions', () => {
     cy.wait(10000);
     cy.get('.cart-price').within(() => {
         cy.contains('.price',"R$45,00")
-    });
-  })
-})
+    });*/
+
+    Cypress._.times(100,() => {
+        it('should register a new user', () => {
+          const user = {
+              firstName: "Test",
+              lastName: "Cypress",
+              cpf: "31226664644",
+              email: Math.random().toString(36).substr(2, 9) + "@gmail.com",
+              password: Math.random().toString(36)
+          }
+
+          cy.visit('https://stg-magento2.mundipagg.com/customer/account/create');
+          cy.get("#firstname").type(user.firstName);
+          cy.get("#lastname").type(user.lastName);
+          cy.get("#taxvat").type(user.cpf);
+          cy.get("#lastname").type(user.lastName);
+          cy.get("#email_address").type(user.email);
+          cy.get("#password").type(user.password);
+          cy.get("#password-confirmation").type(user.password);
+          cy.get('.actions-toolbar').within(() => {
+            cy.get("button[title='Create an Account']").click();
+          });
+
+          cy.get('.block-title').within(() => {
+            cy.contains('strong',"Account Information")
+          });
+
+
+        })
+    })
 })
